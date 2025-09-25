274179d3007c505ccdaafe9467c20804815e2ce5
CHIAO919 <crazy520ky0919@gmail.com>
2025-09-25 12:30:39 +0800

    feat: Add faChevronDown icon to Font Awesome library

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -2,9 +2,9 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 // 挑選要用的 icon
-import { faUser, faHome, faUserPlus, faRightToBracket, faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faHome, faUserPlus, faRightToBracket, faBars, faXmark, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faCircleUser } from "@fortawesome/free-regular-svg-icons";
 
 // 把 icon 放進 library
-library.add(faUser, faHome, faGithub, faUserPlus, faCircleUser, faRightToBracket, faBars, faXmark)
+library.add(faUser, faHome, faGithub, faUserPlus, faCircleUser, faRightToBracket, faBars, faXmark, faChevronDown)
