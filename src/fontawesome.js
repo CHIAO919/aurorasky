c71c2021bacbbbd54bd658eea17b676315783238
CHIAO919 <crazy520ky0919@gmail.com>
2025-10-13 16:06:21 +0800

    feat: Add faArrowRight icon to FontAwesome library

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -4,9 +4,9 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
 // 挑選要用的 icon
-import { faUser, faHome, faUserPlus, faRightToBracket, faBars, faXmark, faChevronDown, faChevronRight, faChevronLeft, faIdCard, faGift, faSuitcase, faPersonWalkingLuggage, faPlaneDeparture, faGaugeSimpleHigh, faUsers, faDoorOpen, faPlane, faPhone, faPhoneVolume, faLocationDot, faCircleExclamation, faFileCirclePlus, faFileCircleCheck, faPlaneUp, faPlaneCircleCheck, faPlaneArrival } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faHome, faUserPlus, faRightToBracket, faBars, faXmark, faChevronDown, faChevronRight, faChevronLeft, faIdCard, faGift, faSuitcase, faPersonWalkingLuggage, faPlaneDeparture, faGaugeSimpleHigh, faUsers, faDoorOpen, faPlane, faPhone, faPhoneVolume, faLocationDot, faCircleExclamation, faFileCirclePlus, faFileCircleCheck, faPlaneUp, faPlaneCircleCheck, faPlaneArrival, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faCircleUser, faFile } from "@fortawesome/free-regular-svg-icons";
 
 // 把 icon 放進 library
-library.add(faUser, faHome, faGithub, faUserPlus, faCircleUser, faRightToBracket, faBars, faXmark, faChevronDown, faChevronRight, faChevronLeft, faIdCard, faGift, faSuitcase, faPersonWalkingLuggage, faPlaneDeparture, faGaugeSimpleHigh, faUsers, faDoorOpen, faPlane, faPhone, faPhoneVolume, faLocationDot, faCircleExclamation, faFile, faFileCirclePlus, faFileCircleCheck, faPlaneUp, faPlaneCircleCheck, faPlaneArrival);
+library.add(faUser, faHome, faGithub, faUserPlus, faCircleUser, faRightToBracket, faBars, faXmark, faChevronDown, faChevronRight, faChevronLeft, faIdCard, faGift, faSuitcase, faPersonWalkingLuggage, faPlaneDeparture, faGaugeSimpleHigh, faUsers, faDoorOpen, faPlane, faPhone, faPhoneVolume, faLocationDot, faCircleExclamation, faFile, faFileCirclePlus, faFileCircleCheck, faPlaneUp, faPlaneCircleCheck, faPlaneArrival, faArrowRight);
