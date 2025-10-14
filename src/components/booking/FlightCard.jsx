9175b0525ba12dc18d98d9d1baab89d618f0ddd3
CHIAO919 <crazy520ky0919@gmail.com>
2025-10-14 15:26:53 +0800

    refactor: Improve layout and spacing in FlightCard component

--- src/components/booking/FlightCard.jsx
+++ src/components/booking/FlightCard.jsx
@@ -30,19 +30,19 @@ export default function FlightCard({ item, onSelect }) {
 
     return (
         <article className='flex justify-between px-7 shadow-sm bg-white rounded-2xl'>
-            <div className='flex items-center justify-between flex-1 text-text-blue px-6 py-4 border-r border-gray-400'>
-                <div className='text-center text-lg font-bold'>
+            <div className='flex items-center justify-between flex-1 text-text-blue px-6 py-8 border-r border-gray-400'>
+                <div className='flex flex-col items-center gap-1.5 text-center text-lg font-bold'>
                     <p>{originName}</p>
                     <p>{departureTime}</p>
                 </div>
 
-                <div className='text-center text-sm font-light text-text-blue'>
+                <div className='flex flex-col items-center gap-1.5 text-center text-sm font-light text-text-blue'>
                     <p>{flightNo}</p>
                     <FontAwesomeIcon icon={["fas", "arrow-right"]} />
                     <p>3小時10分鐘</p>
                 </div>
 
-                <div className='text-center text-lg font-bold text-text-blue'>
+                <div className='flex flex-col items-center gap-1.5 text-center text-lg font-bold text-text-blue'>
                     <p>{destinationName}</p>
                     <p>
                         {arrivalTime}
