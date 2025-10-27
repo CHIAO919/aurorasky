d444abb92d54b5b1c3110a6d95c2543ebb468231
CHIAO919 <crazy520ky0919@gmail.com>
2025-10-27 10:30:18 +0800

    feat: Refactor BaggageCard layout for improved spacing and text styling

--- src/components/booking/BaggageCard.jsx
+++ src/components/booking/BaggageCard.jsx
@@ -9,19 +9,19 @@ export default function BaggageCard({ title, leg, desc }) {
     } = item;
 
     return (
-        <div className="text-text-blue bg-white p-8 rounded-2xl">
-            <h3 className="text-xl font-bold mb-[15px]">{title}</h3>
-            <div className="grid grid-cols-2 px-8 text-center font-semibold">
+        <div className="text-text-blue bg-white py-8 px-4 md:px-8 rounded-2xl">
+            <h3 className="text-lg md:text-xl font-bold mb-[15px]">{title}</h3>
+            <div className="grid grid-cols-2 px-4 md:px-8 text-center">
                 <div className="w-[100%]">
                     <p className="pb-2 border-b border-gray-400">{originName}－{destinationName}</p>
-                    <div className="flex items-center justify-center gap-2 pt-2">
+                    <div className="flex items-center justify-center gap-1 pt-2">
                         <FontAwesomeIcon icon={['fas', 'check']} className="text-red-600"/>
                         <p>{desc}</p>
                     </div>
                 </div>
                 <div className="w-[100%]">
                     <p className="pb-2 border-b border-gray-400">{destinationName}－{originName}</p>
-                    <div className="flex items-center justify-center gap-2 pt-2">
+                    <div className="flex items-center justify-center gap-1 pt-2">
                         <FontAwesomeIcon icon={['fas', 'check']} className="text-red-600"/>
                         <p>{desc}</p>
                     </div>
